1ba40c710753d81786f2b92e2b82da101a889c57
thomas <thomas@madebyon.com>
2022-01-22 19:00:21 +0000

    rmv

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,9 @@ import Homepage from '../components/homepage'
 
 
 const Home = (props) => {
-  console.log(props)
   return <Homepage />
 }
 
 export default Home;
 
 
-
-export async function getServerSideProps(context) {
-  
-  let guestbook = process.env['GUESTBOOK']
-  let token = process.env['DROPBOX_TOKEN']
-
-  return {
-    props: {
-      guestbook, token
-    }, 
-  }
-}
-  
