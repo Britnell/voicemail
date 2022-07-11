8968b5c5be500fe8f23316017b1a34c377e0eb19
Britnell <t.a.britnell@gmail.com>
2022-07-11 09:16:59 +0200

    reminder message

--- components/record/record.js
+++ components/record/record.js
@@ -104,6 +104,9 @@ function Record({ token, back }) {
       </div>
       {/* Footer Buttons  */}
       <div className={styles.footer}>
+        <dic className={styles.reminder}>
+          On Smartphone please keep your finger on the screen to keep your phone unlocked.
+        </dic>
         {state === "ready" && <button onClick={playIntro}>start</button>}
         {state === "intro" && (
           <button>
